b7db39a63ad83ab37eef5f4a6a41b7bb8bbde807
Ava Barron <mztriz@gmail.com>
2020-10-26 16:33:09 -0400

    Create notifier.py

---
+++ app/notifier.py
@@ -0,0 +1,14 @@
+from is_online import is_online
+from send_sms import send_sms
+
+# Players to get notified on
+playerids = [""]
+
+while True:
+    for playerid in playerids:
+        if is_online(playerid):
+                print(f"Player {playerid} is online")
+                send_sms(playerid)
+                playerids.remove(playerid)
+        else:
+            pass
