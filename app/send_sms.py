506253749176027086f0e8d78fa9a7ced6d95c3a
Ava Barron <mztriz@gmail.com>
2021-10-26 11:13:48 -0400

    Update send_sms.py

--- app/send_sms.py
+++ app/send_sms.py
@@ -6,4 +6,4 @@ def send_sms(playerid):
     client = Client(os.getenv("twilio_sid"), os.getenv("twilio_token"))
     client.messages.create(to=os.getenv("twilio_tnum"),
                         from_=os.getenv("twilio_fnum"),
-                        body=f"Player {playerid} is online!")
+                        body=f"The player {playerid} is online!")
