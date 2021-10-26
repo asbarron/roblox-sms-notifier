4daced88ba7b7b2328fd60809ea15df85c1021f9
Thomas Schmidt <thomas.schmidt@gastfreund.net>
2021-10-26 17:56:17 +0200

    Issue-3 Add .gitignore, rework notifier according to AC, add print statement to send_sms

--- app/notifier.py
+++ app/notifier.py
@@ -1,14 +1,42 @@
 from is_online import is_online
 from send_sms import send_sms
-
+import time
 # Players to get notified on
 playerids = [""]
 
+online = [""]
+offline = [""]
+
+print("---Initial Check ---")
+for playerid in playerids:
+    if is_online(playerid):
+        print(f"Player {playerid} is online")
+        send_sms(playerid)
+        online.append(playerid)
+    else:
+        print(f"Player {playerid} is offline")
+        offline.append(playerid)
+print("--- Initial Check done ---")
+
 while True:
     for playerid in playerids:
-        if is_online(playerid):
-                print(f"Player {playerid} is online")
+        if playerid in offline:
+            if is_online(playerid):
+                print(f" Player {playerid} was offline and is now online")
                 send_sms(playerid)
-                playerids.remove(playerid)
+                offline.remove(playerid)
+                online.append(playerid)
+            else:
+                print(f"Player {playerid} was offline and is still offline")
+                pass
         else:
-            pass
+            if is_online(playerid) == False:
+                print(f"Player {playerid} was online and is now offline")
+                online.remove(playerid)
+                offline.append(playerid)
+            else:
+                print(f"Player {playerid} was online and is still online")
+                pass
+
+    time.sleep(5)
+    print("--- Next round ---")
--- app/send_sms.py
+++ app/send_sms.py
@@ -1,8 +1,9 @@
-from twilio.rest import Client
-import settings
+#from twilio.rest import Client
+#import settings
 import os
 
-def send_sms(playerid):
+def send_sms2(playerid):
+    print("send sms" + playerid)
     client = Client(os.getenv("twilio_sid"), os.getenv("twilio_token"))
     client.messages.create(to=os.getenv("twilio_tnum"),
                         from_=os.getenv("twilio_fnum"),
